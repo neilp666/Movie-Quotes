04fba601d71e30a8ccd218ab6f1287224a9ef167
neil patel <neil@myindianweddingapp.com>
2014-04-12 13:14:32 +0100

    Removed Json references as not required

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,64 +1,53 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
+  
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
+
   def show
   end
 
-  # GET /posts/new
+  
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
+  
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
+  
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
+  
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
+        redirect_to @post, notice: 'Post was successfully created.'
+        
       else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render action: 'new' 
+        
       end
-    end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+
   def update
-    respond_to do |format|
+   
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @post, notice: 'Post was successfully updated.' 
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render action: 'edit'
+  
       end
-    end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+ 
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
+      redirect_to posts_url
   end
 
   private
